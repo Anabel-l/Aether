b934935a1dc00d399f04319c290ea764bca76ee8
Aaron Ridley <ridley@umich.edu>
2023-06-30 12:07:30 -0400

    FEAT: changed how the run set-ups are done

--- tests/read_chemistry/run_test.sh
+++ tests/read_chemistry/run_test.sh
@@ -1,24 +1,25 @@
-#! /usr/bin/bash
+#!/bin/sh
 
 # remove old directory:
-rm -rf run.no_errors
+rm -rf run.no_errors run.errors
 
 # run without any errors to check it works:
 cp -R ../../share/run ./run.no_errors
 cd run.no_errors
+cp ../aether.json.no_errors ./aether.json
 ./aether
+cd ../
+
+
 
 #now we get into the real testing
-cd ../
-cd run.errors/UA/inputs
+cp -R ../../share/run ./run.errors
+cd run.errors
 for i in 0 1 2 3 4 5; 
 do 
     echo
     echo Test $i: checking for successful crash 
     echo ......................
-    sed -i "s/chemistry_earth_richards.csv/chemistry_check_$i.csv/g" defaults.json; 
-    cd ../../
-    ./aether;
-    cd UA/inputs;
-    sed -i "s/chemistry_check_$i.csv/chemistry_earth_richards.csv/g" defaults.json; 
+    cp ../aether.check_$i ./aether.json
+    mpirun -np 4 ./aether;
 done;
