049307015221cdf82bd4743440b4a7ecdf0d6fc7
loucks <loucks@umich.edu>
2023-06-30 11:26:48 -0400

    sed method of testing

--- tests/read_chemistry/run_test.sh
+++ tests/read_chemistry/run_test.sh
@@ -10,16 +10,16 @@ cp ../aether.json.no_errors ./aether.json
 ./aether
 cd ../
 
-
-
 #now we get into the real testing
 cp -R ../../share/run ./run.errors
 cd run.errors
 for i in 0 1 2 3 4 5; 
-do 
+do
     echo
     echo Test $i: checking for successful crash 
     echo ......................
+    sed -i "s/chemistry_earth_richards.csv/chemistry_check_$i.csv/g" aether.json
     cp ../aether.check_$i ./aether.json
-    mpirun -np 4 ./aether;
+    mpirun -np 4 ./aether
+    sed -i "s/chemistry_check_$i.csv/chemistry_earth_richards.csv/g" aether.json
 done;
